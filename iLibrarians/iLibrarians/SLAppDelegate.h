76fdb8f80d3d435f800e9cf588d093b5010760c9
陈志炯 <johnsonpuning@gmail.com>
2014-07-08 14:32:17 +0800

    Xcode Project

---
+++ iLibrarians/iLibrarians/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  iLibrarians
+//
+//  Created by johnson on 14-7-8.
+//  Copyright (c) 2014年 Apple Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iLibrarians/iLibrarians/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iLibrarians
+//
+//  Created by johnson on 14-7-8.
+//  Copyright (c) 2014年 Apple Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SLAppDelegate class]));
+    }
+}
