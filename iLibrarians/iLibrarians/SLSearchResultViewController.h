78379441d2f8d8a3d83279604943f212617f20d5
Hahn.Chan <porridgec@gmail.com>
2014-07-15 22:40:34 +0800

    add SeachResultViewController

---
+++ iLibrarians/iLibrarians/SLSearchResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLSearchResultViewController.h
+//  iLibrarians
+//
+//  Created by Hahn.Chan on 7/15/14.
+//  Copyright (c) 2014 Apple Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLSearchResultViewController : UIViewController
+
+@end
