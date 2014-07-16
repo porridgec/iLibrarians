fcc2015de5e6f0e4989924707e067ee06cc8b992
Hahn.Chan <porridgec@gmail.com>
2014-07-16 23:53:46 +0800

    将search页面改为UIView

---
+++ iLibrarians/iLibrarians/SLSearchBookView.h
@@ -0,0 +1,13 @@
+//
+//  SLSearchBookView.h
+//  iLibrarians
+//
+//  Created by Hahn.Chan on 7/16/14.
+//  Copyright (c) 2014 Apple Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLSearchBookView : UIView
+
+@end
