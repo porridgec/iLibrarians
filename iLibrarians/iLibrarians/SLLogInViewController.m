1657046829f1f2ef411023681448665049bf6970
Hahn.Chan <porridgec@gmail.com>
2014-07-17 00:16:32 +0800

    删除所有SearchBookVC

--- iLibrarians/iLibrarians/SLLogInViewController.m
+++ iLibrarians/iLibrarians/SLLogInViewController.m
@@ -1,7 +1,6 @@
 #import "SLAppDelegate.h"
 #import "SLLoginViewController.h"
 #import "SLMyLibraryViewController.h"
-#import "SLSearchBookViewController.h"
 #import "SLBooksExchangeViewController.h"
 #import "SLMyInfoViewController.h"
 #import "SLMainViewController.h"
